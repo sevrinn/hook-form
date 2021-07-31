72d64052640ea45e1742cebf9b01144eea8e11b9
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-31 10:04:50 -0700

    removed unnecessary component from main render adn changed name of component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 // import HookForm from './components/HookForm'
-import ConditionalRendering from './components/ConditonalRendering'
+import Form from './components/ConditonalRendering'
 import MovieForm from './components/HandlingValidations';
-import HookForm from './components/HookForm';
+// import HookForm from './components/HookForm';
 
 function App() {
   return (
     <div className="App">
-      <HookForm />
-      <ConditionalRendering />
+      
+      <Form />
       <MovieForm />
     </div>
   );
